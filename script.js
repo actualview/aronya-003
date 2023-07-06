2903cfb63365a6ef230646d3a96d5d5fbad92ab5
actualview <138713344+actualview@users.noreply.github.com>
2023-07-06 23:09:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function renderPlaces(places) {
         model.setAttribute('gltf-model', './assets/trex/scene.gltf');
         model.setAttribute('rotation', '0 10 0');
         model.setAttribute('animation-mixer', '');
-        model.setAttribute('scale', '0.9 0.9 0.9');
+        model.setAttribute('scale', '0.3 0.3 0.3');
 
         model.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
