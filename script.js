79545ee11acb207da09fe50691d12f7cbd4ae28f
actualview <138713344+actualview@users.noreply.github.com>
2023-07-06 21:29:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -16,14 +16,14 @@ function staticLoadPlaces() {
 }
 
 function renderPlaces(places) {
-    let scene = document.querySelector('a-scene');
+    let scene = document.querySelector('trex_08');
 
     places.forEach((place) => {
         let latitude = place.location.lat;
         let longitude = place.location.lng;
 
         let model = document.createElement('a-entity');
-        model.setAttribute('gps-entity-place', `latitude: ${35.148470}; longitude: ${129.064439};`);
+        model.setAttribute('gps-entity-place', `latitude: ${35.149299}; longitude: ${129.065956};`);
         model.setAttribute('gltf-model', './assets/trex/trex_08.gltf');
         model.setAttribute('rotation', '0 180 0');
         model.setAttribute('animation-mixer', '');
